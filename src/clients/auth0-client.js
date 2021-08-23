3ab752450e414f226aca3fb76aa38774449c4737
teohen <teodorohenriquecarvalho@gmail.com>
2021-08-23 20:50:27 -0300

    feat(auth0-client): creates the auth0client

---
+++ src/clients/auth0-client.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { AUTH0_URL, AUTH0_AUDIENCE } = process.env
+
+const getToken = async (clientId, clientSecret) => {
+  try {
+    const response = await axios.post(AUTH0_URL, {
+      client_id: clientId,
+      client_secret: clientSecret,
+      audience: AUTH0_AUDIENCE,
+      grant_type: 'client_credentials'
+    })
+    return response.data;
+  } catch (err) {
+    console.error('auth0-client - error trying to generate a new token')
+    console.error('Error ', err.message)
+    throw new Error('Can´t generate a new token')
+  }
+}
+
+export default {
+  getToken
+}
